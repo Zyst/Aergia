5faa76d6043e280305fb808c74c234748928db61
Zyst <erick.romero.dev@gmail.com>
2017-07-11 20:02:12 -0500

    Initial commit with starter

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Aergia
 A pomodoro timer made with electron
+
+Kickstarted with [electron-react-boilerplate](https://github.com/chentsulin/electron-react-boilerplate)
---
+++ app/components/LoggedIn.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+export default class LoggedIn extends Component {
+  render() {
+    return (
+      <div>
+        <h2>
+          Logged in as {this.props.user.username}
+        </h2>
+      </div>
+    );
+  }
+}
