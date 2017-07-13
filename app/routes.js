90f4476d9c26ffbce31a3fc1e8eeed8009c142ac
Zyst <erick.romero.dev@gmail.com>
2017-07-13 13:45:40 -0500

    Does it?

--- app/routes.js
+++ app/routes.js
@@ -1,16 +1,14 @@
 /* eslint flowtype-errors/show-errors: 0 */
-import React from 'react';
-import { Switch, Route } from 'react-router';
-import App from './containers/App';
-import HomePage from './containers/HomePage';
-import CounterPage from './containers/CounterPage';
+import React from "react";
+import { Switch, Route } from "react-router";
+import App from "./containers/App";
+import HomePage from "./containers/HomePage";
+import CounterPage from "./containers/CounterPage";
 
-export default () => (
+export default () =>
   <App>
-    <h1>Sup</h1>
     <Switch>
       <Route path="/counter" component={CounterPage} />
       <Route path="/" component={HomePage} />
     </Switch>
-  </App>
-);
+  </App>;
