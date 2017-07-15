4694aa608520e0e1fabf4b4e2915a7cc266b38b7
Zyst <erick.romero.dev@gmail.com>
2017-07-14 21:36:31 -0500

    marked that this is a placeholder because my friends are dumb

--- app/components/AddInput.js
+++ app/components/AddInput.js
@@ -1,5 +1,6 @@
 import React from "react";
 
-const AddInput = () => <input type="text" placeholder="Task input" />;
+const AddInput = () =>
+  <input type="text" placeholder="Task input placeholder" />;
 
 export default AddInput;
