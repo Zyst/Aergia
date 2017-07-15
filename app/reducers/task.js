903dd993fb6524bf06ced62066de83be5c0af420
Zyst <erick.romero.dev@gmail.com>
2017-07-14 23:20:41 -0500

    Task reducer yo!

---
+++ app/reducers/task.js
@@ -0,0 +1,43 @@
+// @flow
+import { ADD_TASK, REMOVE_TASK } from "../actions/task";
+
+export type taskStateType = Array<{
+  +name: string,
+  currentProgress: number,
+  totalPomodoros: number,
+  stopped: number,
+  active: boolean
+}>;
+
+type actionType = {
+  +type: string,
+  +name: string,
+  +totalPomodoros: number
+};
+
+export default function tasks(
+  state: taskStateType,
+  action: actionType
+): taskStateType {
+  switch (action.type) {
+    case ADD_TASK:
+      // If a task with that name doesn't exist already
+      if (state.filter(task => task.name === action.name).length === 0) {
+        return [
+          ...state,
+          {
+            name: action.name,
+            currentProgress: 0,
+            totalPomodoros: action.totalPomodoros,
+            stopped: 0,
+            active: false
+          }
+        ];
+      }
+      return state;
+    case REMOVE_TASK:
+      return state.filter(task => task.name !== action.name);
+    default:
+      return state;
+  }
+}
