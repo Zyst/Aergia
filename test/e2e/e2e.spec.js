bea9882344e49ed04544fde678159d3aed6150d0
Zyst <erick.romero.dev@gmail.com>
2017-07-15 21:19:30 -0500

    Alright, we are gonna write e2e, but at the end

--- test/e2e/e2e.spec.js
+++ test/e2e/e2e.spec.js
@@ -22,20 +22,13 @@ describe("main window", function spec() {
     }
   });
 
-  const findCounter = () => this.app.client.element('[data-tid="counter"]');
-
-  const findButtons = async () => {
-    const { value } = await this.app.client.elements('[data-tclass="btn"]');
-    return value.map(btn => btn.ELEMENT);
-  };
-
   it("should open window", async () => {
     const { client, browserWindow } = this.app;
 
     await client.waitUntilWindowLoaded();
     await delay(500);
     const title = await browserWindow.getTitle();
-    expect(title).toBe("Hello Electron React!");
+    expect(title).toBe("Aergia");
   });
 
   it("should haven't any logs in console of main window", async () => {
