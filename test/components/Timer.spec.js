8e3ac1c823b1305c7ac11fffef25f553e8b19c7c
Zyst <erick.romero.dev@gmail.com>
2017-07-19 12:38:25 -0500

    Back to capitalized, convention is stupid anyway

--- test/components/Timer.spec.js
+++ test/components/Timer.spec.js
@@ -1,8 +1,8 @@
-import { spy } from "sinon";
+// import { spy } from "sinon";
 import React from "react";
 import { shallow } from "enzyme";
-import { BrowserRouter as Router } from "react-router-dom";
-import renderer from "react-test-renderer";
+// import { BrowserRouter as Router } from "react-router-dom";
+// import renderer from "react-test-renderer";
 import Timer, { zeroPadding, displayTime } from "../../app/components/Timer";
 
 const setup = () => {
@@ -15,44 +15,49 @@ const setup = () => {
 };
 
 describe("Timer component", () => {
-  it("should be defined", () => {
+  it("Should be defined", () => {
     expect(Timer).toBeDefined();
   });
 
-  it("");
+  it("Should display a timer", () => {
+    const { timer } = setup();
+    expect(timer.text()).toBe("25:00");
+  });
+
+  it("Should match snapshot");
 
   describe("Zero padding", () => {
-    it("should be defined", () => {
+    it("Should be defined", () => {
       expect(zeroPadding).toBeDefined();
     });
 
-    it("should pad zeroes", () => {
+    it("Should pad zeroes", () => {
       expect(zeroPadding(0)).toBe("00");
       expect(zeroPadding(9)).toBe("09");
     });
 
-    it("should ignore numbers that don't need to be padded", () => {
+    it("Should ignore numbers that don't need to be padded", () => {
       expect(zeroPadding(10)).toBe("10");
       expect(zeroPadding(1992)).toBe("1992");
     });
   });
 
   describe("Display time", () => {
-    it("should be defined", () => {
+    it("Should be defined", () => {
       expect(displayTime).toBeDefined();
     });
 
-    it("should return seconds as seconds with 00 minutes", () => {
+    it("Should return seconds as seconds with 00 minutes", () => {
       expect(displayTime(0)).toBe("00:00");
       expect(displayTime(59)).toBe("00:59");
     });
 
-    it("should return minutes properly", () => {
+    it("Should return minutes properly", () => {
       expect(displayTime(3540)).toBe("59:00");
       expect(displayTime(3599)).toBe("59:59");
     });
 
-    it("should return hours properly (No Zero pad)", () => {
+    it("Should return hours properly (No Zero pad)", () => {
       expect(displayTime(3666)).toBe("1:01:06");
       expect(displayTime(36000)).toBe("10:00:00");
     });
