b37b14019b2d3593b34bebadf939e1839cdce331
Zyst <erick.romero.dev@gmail.com>
2017-07-21 22:06:52 -0500

    Autofocusing this is legit good, I swear, also starting this comp

--- app/components/AddInput.js
+++ app/components/AddInput.js
@@ -1,7 +1,25 @@
 // @flow
-import React from "react";
+import React, { Component } from "react";
 
-const AddInput = () =>
-  <input type="text" placeholder="Task input placeholder" />;
+type Props = {
+  cancel: () => void
+};
+
+class AddInput extends Component {
+  props: Props;
+
+  constructor(props: Props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <form>
+        {/* eslint-disable jsx-a11y/no-autofocus */}
+        <input type="text" autoFocus placeholder="Task name" />;
+      </form>
+    );
+  }
+}
 
 export default AddInput;
