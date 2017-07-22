232f7f91884a9a4a0393f32ea0df812351b45064
Zyst <erick.romero.dev@gmail.com>
2017-07-21 21:30:48 -0500

    This should padd state down

--- app/containers/Add.js
+++ app/containers/Add.js
@@ -1,18 +1,30 @@
 // @flow
-import React from "react";
+import React, { Component } from "react";
 import AddButton from "../components/AddButton";
 import AddInput from "../components/AddInput";
 
-const Add = () => {
-  // This will later on change, only one of these will be active
-  // at a time
-  const lolol = (
-    <div>
-      {true ? <AddButton /> : <AddInput />}
-    </div>
-  );
-
-  return lolol;
+type State = {
+  adding: boolean
 };
+
+class Add extends Component {
+  state: State;
+
+  constructor() {
+    super();
+
+    this.state = {
+      adding: false
+    };
+  }
+
+  render() {
+    const adding = this.state.adding
+      ? <AddInput adding={this.state.adding} />
+      : <AddButton adding={this.state.adding} />;
+
+    return adding;
+  }
+}
 
 export default Add;
