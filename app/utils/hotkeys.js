39ac0eec5127a197e6516e9131432c0e3409ed66
Zyst <erick.romero.dev@gmail.com>
2017-07-26 16:19:58 -0500

    Removed gitkeep, added hotkeys structure

---
+++ app/utils/hotkeys.js
@@ -0,0 +1,24 @@
+export default {
+  Pomodoro: {
+    Add: {
+      AddButton: {
+        callAdd: {
+          key: "<Enter>"
+        }
+      },
+      AddInput: {
+        callCancel: {
+          key: "<Esc>"
+        },
+        incrementPomodoros: {
+          key: "Up"
+        },
+        decrementPomodoros: {
+          key: "Down"
+        }
+      }
+    },
+    Tasks: {}
+  },
+  Options: {}
+};
