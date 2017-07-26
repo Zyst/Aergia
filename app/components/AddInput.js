e242c7ac32f244090e2f7b59aa8243655ebc0ba7
Zyst <erick.romero.dev@gmail.com>
2017-07-26 13:41:00 -0500

    Reformat AddInput to have state

--- app/components/AddInput.js
+++ app/components/AddInput.js
@@ -1,5 +1,5 @@
 // @flow
-import React from "react";
+import React, { Component } from "react";
 import { connect } from "react-redux";
 import { addTask } from "../actions/task";
 import type { actionType } from "../reducers/task";
@@ -9,36 +9,52 @@ type Props = {
   dispatch: (action: actionType) => void
 };
 
-/* eslint-disable import/no-mutable-exports */
-let AddInput = ({ cancel, dispatch }: Props) => {
-  let task: ?HTMLInputElement;
-
-  return (
-    <form
-      onSubmit={e => {
-        e.preventDefault();
-
-        if (task && task.value.trim()) {
-          dispatch(addTask(task.value.trim(), 1));
-
-          task.value = "";
-
-          cancel();
-        }
-      }}
-    >
-      {/* eslint-disable jsx-a11y/no-autofocus */}
-      <input
-        ref={node => {
-          task = node;
-        }}
-        type="text"
-        autoFocus
-        placeholder="Task name"
-      />;
-    </form>
-  );
+type State = {
+  pomodoros: number
 };
+
+class AddInput extends Component {
+  props: Props;
+  state: State;
+
+  constructor(props: Props) {
+    super(props);
+
+    this.state = {
+      pomodoros: 0
+    };
+  }
+
+  render() {
+    let task: ?HTMLInputElement;
+    return (
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+
+          if (task && task.value.trim()) {
+            this.props.dispatch(addTask(task.value.trim(), 1));
+
+            task.value = "";
+
+            this.props.cancel();
+          }
+        }}
+      >
+        {/* eslint-disable jsx-a11y/no-autofocus */}
+        <input
+          ref={node => {
+            task = node;
+          }}
+          type="text"
+          autoFocus
+          placeholder="Task name"
+        />;
+      </form>
+    );
+  }
+}
+/* eslint-disable no-class-assign */
 AddInput = connect()(AddInput);
 
 export default AddInput;
